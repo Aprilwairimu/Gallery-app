9cd52af5375b60a9db7c7175496e8fe6d34991cd
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-28 15:56:59 +0300

    created a gallery.html

--- gallery/views.py
+++ gallery/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from django.http  import HttpResponse,Http404
 # Create your views here.
 
+def gallery(request):
+    return render(request,'photos/gallery.html')
+
+def ViewPhoto(request):
+    return render(request,'photos/photo.html')
+
+def addPhoto(request):
+    return render(request,'photos/add.html')
