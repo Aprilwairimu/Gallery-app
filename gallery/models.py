aff907677741c3f93716c9fcdd89eb4cbeb143ea
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-29 20:55:55 +0300

    added Category,Location and photo model classes

--- gallery/models.py
+++ gallery/models.py
@@ -8,28 +8,51 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+ # def save_photo(self):
+    #     self.save()
+    
+    # def delete_photo(self):
+    #     self.save()
 
+    # def update_photo(self):
+    #     self.save()
+
+class Location(models.Model):
+    location = models.CharField(max_length=100,null=False, blank=False)
+
+ # def save_photo(self):
+    #     self.save()
+    
+    # def delete_photo(self):
+    #     self.save()
+
+    # def update_photo(self):
+    #     self.save()
 
 class Photo(models.Model):
+    image = models.ImageField (null=False, blank=False)
     name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=100)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.description
 
-    def save_image(self):
-        self.save()
+    # def save_photo(self):
+    #     self.save()
     
-    def delete_image(self):
-        self.save()
+    # def delete_photo(self):
+    #     self.save()
 
-    def update_image(self):
-        self.save()
+    # def update_photo(self):
+    #     self.save()
 
-    def get_image_by_id(id):
-        self.save()
+    # def get_photo_by_id(id):
+    #     self.save()
 
-    def search_image(Category):
-        self.save()
+    # def search_photo(Category):
+    #     self.save()
 
-    def filter_by_location(location):
-        self.save()
+    # def filter_by_location(location):
+    #     self.save()
