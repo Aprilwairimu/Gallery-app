acaa8727ad73a9bd40c1fee5056b4f88c9bd12b1
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-30 09:24:47 +0300

    added a search function

--- gallery/views.py
+++ gallery/views.py
@@ -19,3 +19,16 @@ def addPhoto(request):
 
     context ={'categories': categories}
     return render(request,'photos/add.html')
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Category.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photos/search.html',{"message":message,"categories": searched_categories})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photos/search.html',{"message":message})
