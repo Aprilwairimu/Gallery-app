c4a3f7ab759ad0aceb6a68649d3a8bdbf3da020b
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-31 09:28:03 +0300

    added a new url path

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns=[
     path('',views.home, name='home',),
     path('gallery',views.gallery, name='gallery',),
-    path('photo/<str:pk>/',views.ViewPhoto, name='photo',),
+    path('photo/<pk>/',views.ViewPhoto, name='photo',),
     path('add/',views.addPhoto, name='add',)
 ]
